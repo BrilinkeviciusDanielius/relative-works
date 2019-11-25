6babc24964bcbc3705ca5f367095b0126b5d7bea
BrilinkeviciusDanielius <brilinkeviciusdanielius@gmail.com>
2019-11-25 13:58:45 +0200

    add new

--- database/seeds/AdminSeeder.php
+++ database/seeds/AdminSeeder.php
@@ -2,6 +2,7 @@
 
 use App\User;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Str;
 
 class AdminSeeder extends Seeder
@@ -14,11 +15,11 @@ class AdminSeeder extends Seeder
     public function run()
     {
         User::create([
-            'email' => 'admin@admin.com',
+            'email' => 'danielius@gmail.com',
             'name' => 'admin',
             'email_verified_at' => now(),
-            'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
-            'remember_token' => Str::random(10),
+            'password' => Hash::make('danielius123'),
+            'remember_token' => Str::random(12),
             'role_id' => 1,
         ]);
     }
